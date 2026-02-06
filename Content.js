c16c3e06407793ca1ceaf719e7487721ce022184
NuclearBlox <nukecorebf@gmail.com>
2026-02-06 11:37:36 -0700

    We have a button

--- Content.js
+++ Content.js
@@ -1,3 +1,5 @@
+let Locked = false;
+
 function waitForElement(selector, callback) {
     const element = document.querySelector(selector);
     if (element) {
@@ -62,12 +64,48 @@ function AddButton(leftControls, Speed) {
         button.style.background = 'var(--yt-spec-overlay-background-medium-light,rgba(0,0,0,.3))';
     });
 }
+function lockButton(leftControls) {
+        const button = document.createElement('button');
 
+    button.style.position = 'relative';
+    button.style.height = '40px';
+    button.style.width = '40px';
+    button.style.fontSize = '14px';
+    button.style.display = 'flex';
+    button.style.alignSelf = 'center';
+    button.style.alignItems = 'center';
+    button.style.justifyContent = 'center';
+    button.style.cursor = 'pointer';
+    button.style.border = 'none';
+    button.style.color = 'var(--yt-spec-text-primary-inverse)';
+    button.style.fontWeight = '500';
+    button.style.borderRadius = '40px';
+    button.style.background = 'var(--yt-spec-overlay-background-medium-light,rgba(0,0,0,.3))';
+    button.style.padding = '8px 16px';
+    button.style.outline = 'none';
+    button.style.color = 'white';
+    button.style.fontFamily = '"YouTube Noto",Roboto,Arial,Helvetica,sans-serif';
+    button.style.fontSize = '14px';
+    button.style.marginLeft = '4px';
+
+
+
+
+    const img = document.createElement('img');
+    img.src = chrome.runtime.getURL('LockIcon.png');
+    img.style.width = '20px';
+    img.style.height = '20px';
+    button.appendChild(img);
+
+        leftControls.appendChild(button);
+}
 
 waitForElement('#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls', (leftControls) => {
     AddButton(leftControls, '.5');
     AddButton(leftControls, '3');
     AddButton(leftControls, '4');
     AddButton(leftControls, '5');
+    console.log("Speed buttons loaded");
+    lockButton(leftControls);
     console.log("Extension loaded");
 });
